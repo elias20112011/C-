6f406510a6e7bd0355322e7dfccd05389db6667e
elias20112011 <alyasalqdmwryalqdmwry@gmail.com>
2024-10-19 16:55:16 +0200

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //out put to the console greeting to the worid
+        print("hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
